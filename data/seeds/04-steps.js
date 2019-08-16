e311b0b1906853cca31e4161e605bc923e58188b
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-15 23:26:20 -0400

    seed files created

--- data/seeds/04-steps.js
+++ data/seeds/04-steps.js
@@ -1,5 +1,16 @@
 exports.seed = function(knex) {
   return knex('steps').insert([
-    //{ description: "step_1", }
+    { description: 'step_1', recipe_id: 1 },
+    { description: 'step_2', recipe_id: 1 },
+    { description: 'step_3', recipe_id: 1 },
+    { description: 'step_1', recipe_id: 2 },
+    { description: 'step_2', recipe_id: 2 },
+    { description: 'step_3', recipe_id: 2 },
+    { description: 'step_4', recipe_id: 2 },
+    { description: 'step_1', recipe_id: 3 },
+    { description: 'step_2', recipe_id: 3 },
+    { description: 'step_1', recipe_id: 4 },
+    { description: 'step_2', recipe_id: 4 },
+    { description: 'step_3', recipe_id: 4 }
   ]);
 };
