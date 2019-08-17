94963528895dde53619c8ea31e1a2afc1639fbdb
Dave Vazquez <dmvaz.li@gmail.com>
2019-08-17 18:01:34 -0400

    GET /api/recipes/:id/shoppingList enpoint implemented

--- routes/recipes/recipeModel.js
+++ routes/recipes/recipeModel.js
@@ -1,11 +1,19 @@
 const db = require('../../data/db-config');
 
 module.exports = {
-  getRecipes
-  // getShoppingList,
+  getRecipes,
+  getShoppingList
   // getInstructions
 };
 
 function getRecipes() {
   return db('recipes');
+}
+
+function getShoppingList(recipe_id) {
+  return db('ingredients as i')
+    .leftJoin('recipes_ingredients as ri', 'i.id', 'ri.ingredient_id')
+    .where('ri.recipe_id', '=', recipe_id)
+    .select('i.name', 'i.qty', 'ri.recipe_id')
+    .orderBy('i.name', 'asc');
 }
--- routes/recipes/recipesRoute.js
+++ routes/recipes/recipesRoute.js
@@ -7,12 +7,33 @@ router.get('/', async (req, res) => {
   try {
     const recipes = await Recipes.getRecipes();
     res.status(200).json({
+      success: true,
       recipes
     });
-  } catch (err) {
+  } catch (error) {
     res.status(500).json({
+      success: false,
       message: 'Internal Server Error: Could not get recipes.',
-      err
+      error
+    });
+  }
+});
+
+router.get('/:id/shoppingList', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const shoppingList = await Recipes.getShoppingList(id);
+    res.status(200).json({
+      success: true,
+      shoppingList
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: 'Internal Server Error: Could not get shopping list.',
+      error
     });
   }
 });
